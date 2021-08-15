a3569f49e7c6ede99165e561fc5522f68ea5f69f
Sudesh <mohansudesh@gmail.com>
2021-08-15 17:11:17 +0530

    initail commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var PORT = 8080;
+
+// Start Server 🚀
+http.listen(PORT, () => {
+	console.log(`Node Server running on port : ${PORT}`);
+});
+
+// Connect to Socket 🔗
+io.on("connection", (socket) => {
+	console.log("Connection established");
+	socket.on("editor-data", (data) => {
+		socket.broadcast.emit("editor-data", data);
+	});
+});
